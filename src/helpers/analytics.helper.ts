08f54a2b6ce431c55ad0b236a1f91efa0cc41f5c
Ajay Gupta <ajay.gupta@ajayg.co>
2024-12-24 21:13:16 +0530

    analytics - call sentry methods

--- src/helpers/analytics.helper.ts
+++ src/helpers/analytics.helper.ts
@@ -1,11 +1,21 @@
+// Modules
+import {
+  captureEvent as _captureEvent,
+  captureException as _captureException,
+} from "@sentry/react";
+
 // Capture Exception
 export const captureException = (...args: unknown[]): void => {
+  _captureException(...args);
+
   // eslint-disable-next-line no-console
   console.error(...args);
 };
 
 // Log
 export const captureEvent = (...args: unknown[]): void => {
+  _captureEvent(...args);
+
   // eslint-disable-next-line no-console
   console.log(...args);
 };
